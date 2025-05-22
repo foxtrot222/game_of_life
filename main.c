30d231c859f8004e938994166b58bccd115380be
foxtrot222 <tirthkavathiya@gmail.com>
2025-05-22 19:58:49 +0530

    Add draw function

--- main.c
+++ main.c
@@ -8,8 +8,11 @@ void print_cell(bool alive);
 bool update(int neighbours, bool status);
 void wait();
 int count_neighbours(bool** neighbourhood , int i, int j, int rows, int cols);
+void clear_neighbourhood(bool** current, bool** updated, int rows, int cols);
+void draw(bool** neighbourhood);
 
 int main() {
+  
   setlocale(LC_ALL, "");
   initscr();
   nodelay(stdscr, TRUE);
@@ -18,8 +21,8 @@ int main() {
   keypad(stdscr, TRUE);
   clear();
   int ch;
-  int y = 0 , x = 0;
   
+  bool **temp;
   int rows, cols, i, j;
   getmaxyx(stdscr, rows, cols);
   int neighbours;
@@ -31,44 +34,18 @@ int main() {
   for ( i = 0 ; i < rows ; i++ ) {
     updated_neighbourhood[i] = malloc( cols * sizeof(bool));
   }
-
   for ( i = 0 ; i < rows ; i++ ) {
     for ( j = 0 ; j < cols ; j++) {
       current_neighbourhood[i][j] = false;
     }
   }
 
+  draw(current_neighbourhood);
+  
   while (1) {
-    ch = getch();
-    if ( ch == KEY_UP) {
-      if (y > 0) y--;
-    }
-    if ( ch == KEY_DOWN) {
-      y++;
-    }
-    if ( ch == KEY_LEFT) {
-      if (x > 0) x--;
-    }
-    if ( ch == KEY_RIGHT) {
-      x++;
-    }
-    if ( ch == 'a' ) {
-      current_neighbourhood[y][x] = true;
-      print_cell(true);
-    }
-    if ( ch == '\n' ) {
-      break;
-    }
-    if ( ch == 'q' ) {
-      break;
-    }
-    move(y,x);
-  }
-
-  curs_set(0);
-  while (1) {
-    wait();
     
+    wait();
+   
     for ( i = 0 ; i < rows ; i++ ) {
       for ( j = 0 ; j < cols ; j++) {
 	neighbours = count_neighbours(current_neighbourhood, i, j, rows, cols);
@@ -76,7 +53,7 @@ int main() {
       }
     }
 
-    bool **temp = current_neighbourhood;
+    temp = current_neighbourhood;
     current_neighbourhood = updated_neighbourhood;
     updated_neighbourhood = temp;
     
@@ -93,12 +70,7 @@ int main() {
    
   }
 
-  for ( i = 0 ; i < rows ; i++ ) {
-    free(current_neighbourhood[i]);
-    free(updated_neighbourhood[i]);
-  }
-  free(current_neighbourhood);
-  free(updated_neighbourhood);
+  clear_neighbourhood(current_neighbourhood, updated_neighbourhood, rows, cols);
   refresh();
   endwin();
   return 0;
@@ -141,4 +113,45 @@ int count_neighbours(bool** neighbour , int i, int j, int rows, int cols) {
   if (i > 0 && j < cols - 1 && neighbour[i-1][j+1]) neighbours++;
   if (i > 0 && j > 0 && neighbour[i-1][j-1]) neighbours++;
   return neighbours;
+}
+
+void clear_neighbourhood(bool** current, bool** updated, int rows, int cols) {
+  for ( int i = 0 ; i < rows ; i++ ) {
+    free(current[i]);
+    free(updated[i]);
+  }
+  free(current);
+  free(updated);
+}
+
+void draw(bool** neighbourhood) {
+  int y = 0 , x = 0;
+  int ch;
+  while (1) {
+    ch = getch();
+    if ( ch == KEY_UP) {
+      if (y > 0) y--;
+    }
+    if ( ch == KEY_DOWN) {
+      y++;
+    }
+    if ( ch == KEY_LEFT) {
+      if (x > 0) x--;
+    }
+    if ( ch == KEY_RIGHT) {
+      x++;
+    }
+    if ( ch == 'a' ) {
+      neighbourhood[y][x] = true;
+      print_cell(true);
+    }
+    if ( ch == '\n' ) {
+      break;
+    }
+    if ( ch == 'q' ) {
+      break;
+    }
+    move(y,x);
+  }
+  curs_set(0);
 }
